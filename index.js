c3247c0b4bafd4f06de8c9221f25afbbfa999900
Ahmad Reza <ahmad.alkhwarizmi@gmail.com>
2018-06-18 10:30:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var uppercase, lowercase
+
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
+}
+function whisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
+}
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+}
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = lowercase;
+  console.log(lowercase);
+}
+function sayHiToGrandma(string){
+  if(string.toLowerCase()===string){
+    return 'I can\'t hear you!'
+  }
+  else if(string.toUpperCase()===string){
+    return 'YES INDEED!'
+  }
+}
